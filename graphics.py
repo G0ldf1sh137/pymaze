de0d825651248bc3b34def8a7c9fc392c6e9340d
G0ldf1sh137 <m.rich137@gmail.com>
2024-01-11 12:19:25 -0500

    moved Window class to graphics.py

---
+++ graphics.py
@@ -0,0 +1,26 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height) -> None:
+        self.__root = Tk(className="Test title")
+        self.__root.geometry(f"{width}x{height}")
+        self.canvas = Canvas()
+        self.canvas.pack()
+        self.running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+        print("User exited...")
+
+
+    def close(self):
+        self.running = False
