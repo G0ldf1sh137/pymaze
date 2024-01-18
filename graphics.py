bc2a952550663a37a45fcd3fb4e90f582bf8e95c
G0ldf1sh137 <m.rich137@gmail.com>
2024-01-18 13:59:29 -0500

    added docstrings to methods and added __str__ and __repr__ to classes

--- graphics.py
+++ graphics.py
@@ -2,9 +2,12 @@ from __future__ import annotations
 
 from tkinter import Tk, BOTH, Canvas
 
-
 class Point:
-    def __init__(self, x: int, y: int) -> None:
+    def __init__(
+            self, 
+            x: int | float,
+            y: int | float,
+        ) -> Point:
         self.x = x
         self.y = y
     
@@ -16,14 +19,21 @@ class Point:
 
 
 class Line:
-    def __init__(self, p1: Point, p2: Point) -> None:
+    def __init__(self, p1: Point, p2: Point) -> Line:
         self.p1 = p1
         self.p2 = p2
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"(({self.p1.x},{self.p1.y})-({self.p2.x},{self.p2.y}))"
     
-    def draw(self, canvas: Canvas, fill_color="black"):
+    def __repr__(self) -> str:
+        return f"Line(Point{self.p1}, Point{self.p2})"
+    
+    def draw(self, canvas: Canvas, fill_color="black") -> None:
+        """
+        Takes a Canvas object and optional fill color,
+        draw this line on the canvas from
+        """
         canvas.create_line(
             self.p1.x, self.p1.y, 
             self.p2.x, self.p2.y,
@@ -34,7 +44,7 @@ class Line:
 
 
 class Window:
-    def __init__(self, width, height) -> None:
+    def __init__(self, width, height) -> Window:
         self.__root = Tk()
         self.__root.title("Maze Solver")
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
@@ -42,36 +52,66 @@ class Window:
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
-    def redraw(self):
+    def redraw(self) -> None:
         self.__root.update_idletasks()
         self.__root.update()
     
-    def draw_line(self, line: Line, fill_color="black"):
+    def draw_line(self, line: Line, fill_color="black") -> None:
+        """
+        Takes a Line object and optional fill_color and draw the line to the
+        Window's canvas
+        """
         line.draw(self.__canvas, fill_color=fill_color)
     
-    def wait_for_close(self):
+    def wait_for_close(self) -> None:
+        """
+        Initializes self.__running to True, continue to redraw the window
+        until the window is closed
+        """
         self.__running = True
         while self.__running:
             self.redraw()
         print("User exited...")
     
-    def close(self):
+    def close(self) -> None:
+        """
+        Used as a callback function to self.__root.protocol to set
+        self.__running = False to stop application from redrawing
+        """
         self.__running = False
 
 
 class Cell:
-    def __init__(self, x1: int, y1: int, x2: int, y2: int, win: Window=None) -> None:
+    def __init__(
+            self, 
+            x1: int, 
+            y1: int, 
+            x2: int, 
+            y2: int, 
+            win: Window=None
+        ) -> Cell:
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self._x1 = x1
-        self._y1 = y1
-        self._x2 = x2
-        self._y2 = y2
+        # make x1/y1 top left and x2/y2 bottom right corner from provided coords
+        self._x1 = min(x1, x2)
+        self._y1 = min(y1, y2)
+        self._x2 = max(x1, x2)
+        self._y2 = max(y1, y2)
         self._win = win
+
+    def __repr__(self) -> str:
+        return f"Cell({self._x1}, {self._y1}, {self._x2}, {self._y2})"
     
-    def draw(self):
+    def __str__(self) -> str:
+        return f"(({self._x1},{self._y1})[]({self._x2},{self._y2}))"
+    
+    def draw(self) -> None:
+        """
+        Draws each cell wall as black if the wall exists, or white 
+        (background color) if the wall does not exist
+        """
         if self.has_left_wall:
             self._win.draw_line(self.left_wall)
         else:
@@ -90,69 +130,86 @@ class Cell:
             self._win.draw_line(self.bottom_wall, 'white')
     
     @property
-    def width(self) -> int:
+    def width(self) -> int | float:
+        """width of the cell"""
         return self._x2 - self._x1
     
     @property
-    def height(self) -> int:
+    def height(self) -> int | float:
+        """height of the cell"""
         return self._y2 - self._y1
     
     @property
     def center(self) -> Point:
+        """"center point of the cell"""
         return Point(
             self._x1 + (self.width / 2), 
             self._y1 + (self.height / 2)
         )
     
     @property
-    def top(self) -> int:
+    def top(self) -> int | float:
+
         return self._y1
     
     @property
-    def bottom(self) -> int:
+    def bottom(self) -> int | float:
         return self._y2
     
     @property
-    def left(self) -> int:
+    def left(self) -> int | float:
         return self._x1
     
     @property
-    def right(self) -> int:
+    def right(self) -> int | float:
         return self._x2
     
     @property
     def top_left(self) -> Point:
+        """Point at the top left corner"""
         return Point(self._x1, self._y1)
     
     @property
     def top_right(self) -> Point:
+        """Point at the top right corner"""
         return Point(self._x2, self._y1)
     
     @property
     def bottom_left(self) -> Point:
+        """Point at the bottom left corner"""
         return Point(self._x1, self._y2)
     
     @property
     def bottom_right(self) -> Point:
+        """Point at the bottom right corner"""
         return Point(self._x2, self._y2)
     
     @property
     def top_wall(self) -> Line:
+        """Line on the top of the cell"""
         return Line(self.top_left, self.top_right)
     
     @property
     def bottom_wall(self) -> Line:
+        """Line on the bottom of the cell"""
         return Line(self.bottom_left, self.bottom_right)
     
     @property
     def left_wall(self) -> Line:
+        """Line on the left side of the cell"""
         return Line(self.top_left, self.bottom_left)
     
     @property
     def right_wall(self) -> Line:
+        """Line on the right side of cell"""
         return Line(self.top_right, self.bottom_right)
     
     def draw_move(self, to_cell: Cell, undo=False):
+        """
+        Draws a Line from the center of the current cell, to the center of
+        to_cell. Default line color to red if undo is False, line color is
+        gray if undo-ing move
+        """
         if not undo:
             fill = "red"
         else:
--- maze.py
+++ maze.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 from graphics import Window, Cell
@@ -11,7 +12,8 @@ class Maze:
             num_cols: int,
             cell_size_x: int,
             cell_size_y: int,
-            win: Window=None
+            win: Window=None,
+            seed: int | float | str | bytes | bytearray = None
     ):
         self._x1 = x1
         self._y1 = y1
@@ -20,10 +22,17 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
+        if seed:
+            random.seed(seed)
         self._create_cells()
         self._break_entrance_and_exit()
-
+    
     def _create_cells(self):
+        """
+        Initilizes self._cells as a list of list of Cell objects. The top level
+        list references the column of the maze, the inner list references the
+        row of the Cell
+        """
         self._cells: list[list[Cell]] = []
         for c in range(self._num_cols):
             cell_col: list[Cell] = []
@@ -35,27 +44,34 @@ class Maze:
                 cell = Cell(x1, y1, x2, y2, self._win)
                 cell_col.append(cell)
             self._cells.append(cell_col)
-
+    
         for c in range(self._num_cols):
             for r in range(self._num_rows):
                 self._draw_cell(c, r)
         
-
+    
     def _draw_cell(self, i: int, j: int):
+        """
+        Takes a positional x/y index and draws the cell in this position
+        """
         if self._win:
             self._cells[i][j].draw()
             self._animate()
-
+    
     def _animate(self):
+        """
+        redraw the window and sleep for 0.05 seconds 
+        before drawing the next frame
+        """
         self._win.redraw()
         time.sleep(0.05)
-
+    
     def get_cell(self, x: int, y: int) -> Cell:
+        """Returns a Cell from within the maze based on the x/y index"""
         return self._cells[y][x]
     
     def _break_entrance_and_exit(self):
-        # start_pos = [0, 0]
-        # end_pos = [self._num_rows -1, self._num_cols -1]
+        """Removes the entrance and exit walls from maze"""
         self.get_cell(0, 0).has_top_wall = False
         self._draw_cell(0, 0)
         self.get_cell(self._num_rows -1, self._num_cols -1).has_bottom_wall = False
