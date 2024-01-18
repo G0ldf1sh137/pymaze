a3b6644d71cc57c5d6416d3a31f6c237b84db24d
G0ldf1sh137 <m.rich137@gmail.com>
2024-01-18 12:00:53 -0500

    moved Maze to maze.py

--- graphics.py
+++ graphics.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from tkinter import Tk, BOTH, Canvas
-import time
 
 
 class Point:
@@ -152,52 +151,3 @@ class Cell:
             fill = "gray"
         line = Line(self.center, to_cell.center)
         self._win.draw_line(line, fill_color=fill)
-
-class Maze:
-    def __init__(
-            self,
-            x1: int,
-            y1: int,
-            num_rows: int,
-            num_cols: int,
-            cell_size_x: int,
-            cell_size_y: int,
-            win: Window
-    ):
-        self._x1 = x1
-        self._y1 = y1
-        self._num_rows = num_rows
-        self._num_cols = num_cols
-        self._cell_size_x = cell_size_x
-        self._cell_size_y = cell_size_y
-        self._win = win
-        self._create_cells()
-
-    def _create_cells(self):
-        self._cells: list[list[Cell]] = []
-        for c in range(self._num_cols):
-            cell_col: list[Cell] = []
-            for r in range(self._num_rows):
-                x1 = self._x1 + (r * self._cell_size_x)
-                x2 = x1 + self._cell_size_x
-                y1 = self._y1 + (c * self._cell_size_y)
-                y2 = y1 + self._cell_size_y
-                cell = Cell(x1, y1, x2, y2, self._win)
-                cell_col.append(cell)
-            self._cells.append(cell_col)
-
-        for c in range(self._num_cols):
-            for r in range(self._num_rows):
-                self._draw_cell(c, r)
-        
-
-    def _draw_cell(self, i: int, j: int):
-        self._cells[i][j].draw()
-        self._animate()
-
-    def _animate(self):
-        self._win.redraw()
-        time.sleep(0.05)
-
-    def get_cell(self, x: int, y: int) -> Cell:
-        return self._cells[y][x]
--- tests.py
+++ tests.py
@@ -1,13 +1,14 @@
 import unittest
 
-from graphics import Maze, Window
+from graphics import Window
+from maze import Maze
 
 
 class Tests(unittest.TestCase):
     def test_maze_create_cells(self):
         num_cols = 10
         num_rows = 10
-        win = Window(800,600)
+        win = Window(550,550)
         m1 = Maze(25, 25, num_rows, num_cols, 50, 50, win)
         self.assertEqual(
             len(m1._cells),
